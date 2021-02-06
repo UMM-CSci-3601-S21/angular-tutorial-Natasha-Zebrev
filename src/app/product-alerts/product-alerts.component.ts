49d32caf399a4345d0408afc12061d16bd26549c
Natasha Zebrev <zebre001@morris.umn.edu>
2021-02-05 22:07:39 -0600

    Fix a typo
    Adds a space before import in line 5

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import {Input} from '@angular/core';
 
-import{ Output, EventEmitter } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
