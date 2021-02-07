47238946fe0e7891779eb342f499e058eca2cd50
Natasha Zebrev <zebre001@morris.umn.edu>
2021-02-07 12:01:57 -0600

    Add checkoutForm
    Adds method to get the user's name and address

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -9,6 +9,12 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
 
+  items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder,
