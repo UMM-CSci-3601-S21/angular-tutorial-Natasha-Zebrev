77f8423161ad6bd47e93a41dd01a9612c3a1e077
Natasha Zebrev <zebre001@morris.umn.edu>
2021-02-06 20:38:31 -0600

    Define addToCart method
    Defines the addToCart method within the class in the product-details.component.ts file

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -30,4 +30,9 @@ export class ProductDetailsComponent implements OnInit {
       productIdFromRoute);
   }
 
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
 }
