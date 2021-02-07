de6731cad66f3d1c5e496636b2222c296a7c38d4
Natasha Zebrev <zebre001@morris.umn.edu>
2021-02-06 20:34:39 -0600

    Import cart service
    Imports cart service to the product-details.component.ts file and adds it into the product details component class

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
 
-import { products } from '.../products';;
+import { products } from '../products';
+
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -15,6 +17,7 @@ export class ProductDetailsComponent implements OnInit {
   /* ...*/
   constructor(
   private route: ActivatedRoute,
+  private cartService: CartService
   ){ }
 
   ngOnInit() {
